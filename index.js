82568175e5a33ca300bcd5c3674b2f6980c76870
harrisonmiller <harrison918@aol.com>
2019-04-24 19:04:26 -0400

    Created index page, finished about and project sections

---
+++ index.js
@@ -0,0 +1,27 @@
+$( document ).ready(function() {
+
+  $("#aboutNav").click(function() {
+      $('html, body').animate({
+          scrollTop: $("#about").offset().top
+      }, 1000);
+  });
+
+  $("#projectNav").click(function() {
+      $('html, body').animate({
+          scrollTop: $("#projects").offset().top
+      }, 1000);
+  });
+
+  $("#experienceNav").click(function() {
+      $('html, body').animate({
+          scrollTop: $("#experience").offset().top
+      }, 1000);
+  });
+
+  $("#contactNav").click(function() {
+      $('html, body').animate({
+          scrollTop: $("#contact").offset().top
+      }, 1000);
+  });
+
+});
